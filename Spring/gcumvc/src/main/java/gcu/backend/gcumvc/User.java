113334313b87d90076b1cad073f0489fa57dd2fe
hayamaster <dnfxmfkdbzos@naver.com>
2023-03-31 15:16:21 +0900

    delete .DS_Store

--- Spring/gcumvc/src/main/java/gcu/backend/gcumvc/User.java
+++ Spring/gcumvc/src/main/java/gcu/backend/gcumvc/User.java
@@ -1,2 +1,35 @@
-package gcu.backend.gcumvc;public class user {
+package gcu.backend.gcumvc;
+
+public class User {
+    String name;
+    String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
